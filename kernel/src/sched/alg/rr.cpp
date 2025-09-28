5a80483a5658326f34f9c47a5a2d67ce99b93eed
Shikhar Srivastava <ss504@st-andrews.ac.uk>
2025-09-28 23:04:30 +0100

    [qol] comments + explanations

--- kernel/src/sched/alg/rr.cpp
+++ kernel/src/sched/alg/rr.cpp
@@ -19,39 +19,42 @@ using namespace stacsos::kernel::sched::alg;
 
 void round_robin::add_to_runqueue(tcb &tcb) { 
 	// runqueue_ is a queue full of tcbs. each tcb handles a thread
-	// dprintf("-----------------queueing task---------------\n");
 	runqueue_.enqueue(&tcb);
 }
 
 void round_robin::remove_from_runqueue(tcb &tcb) {
 	// remove jobs from the runqueue_
-	// dprintf("----------------removing queued task-------------\n");
 	runqueue_.remove(&tcb);
 }
 
 
 tcb *round_robin::select_next_task(tcb *current) { 
-	// dprintf("rr call \n");
-
 	if (runqueue_.empty()) {
 		// return nothing if queue empty
 		return nullptr;
 	}
-
-	if (runqueue_.count() == 1) {
-		// return first if queue is 1-long
-		return runqueue_.first();
-	}
-
-	// counter, only for doc and test
+	
 	u64 counter = 0;
 	for (auto *tcb_elem : runqueue_) {
-		dprintf("-------queued thread: %u-------\n", counter);
+		// basically debug information
+		// useful to check if the round robin is actually moving things 'around'
+		// this is the easiest way to check if the round robin is working as intended
+		// that is, if the round robin process works as intended 
+		// or if it works in another way that also happens to work as a scheduler
+		// /usr/mandelbrot is a good way to test this, as is typing
+		// typing uses only one, so it shows that queueing and dequeueing is working
+
+		// In this case the element prints as a number (mem address) and allows us to 
+		// see that the tcb objects go round and round as the round robin scheduler demands
+		dprintf("-------queued thread %u-------\n", counter, tcb_elem);
 		counter++;
 	}
 	dprintf("current threads: %u\n", counter);
-	
-	runqueue_.enqueue(runqueue_.dequeue());
+
+	if (runqueue_.count() != 1) {
+		// return first if queue is 1-long
+		runqueue_.enqueue(runqueue_.dequeue());
+	}
 
 	return runqueue_.first();
 }
